c89100235fde7c6af20cbd2bc4c363b7ace6c4e3
Azus <a.ben.bouhajja@gmail.com>
2023-10-07 10:55:03 +0200

    adding the tresor project and setting maven project configuration

---
+++ src/main/java/application/TresorGameMainApplication.java
@@ -0,0 +1,19 @@
+package application;
+
+import org.apache.log4j.Logger;
+
+import java.io.Serializable;
+
+
+public class TresorGameMainApplication implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    static final Logger logger = Logger.getLogger(TresorGameMainApplication.class);
+
+    public static void main(final String[] args) {
+
+
+    }
+}
+
+
