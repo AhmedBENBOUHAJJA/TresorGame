9e64078cf3886c23110e8551faa85d61c4add3c9
Azus <a.ben.bouhajja@gmail.com>
2023-10-07 11:44:44 +0200

    adding the test code that we hope to run successfully

--- src/main/java/TresorGame/Tresor.java
+++ src/main/java/TresorGame/Tresor.java
@@ -2,13 +2,14 @@ package TresorGame;
 
 import entity.Aventurier;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Tresor {
     private char[][] map; // the cart
     private List<Aventurier> aventuriers; // adventurers list
 
-    // Method to read the input file and initialize the map and adventurers
+// Method to read the input file and initialize the map and adventurers
 
     public void readInputFile(String inputFile) {
         // Read the input file and initialize the map and adventurers
@@ -25,4 +26,37 @@ public class Tresor {
     public void writeOutputFile(String outputFile) {
         // Write the output file
     }
+
+    public char[][] getMap() {
+        return map;
+    }
+
+    public void setMap(char[][] map) {
+        this.map = map;
+    }
+
+    public List<Aventurier> getAventuriers() {
+        return aventuriers;
+    }
+
+    public void setAventuriers(List<Aventurier> aventuriers) {
+        this.aventuriers = aventuriers;
+    }
+
+    public int getMapWidth() {
+        return map != null ? map[0].length : 0;
+    }
+
+    public int getMapHeight() {
+        return map != null ? map.length : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Tresor{" +
+                "map=" + Arrays.toString(map) +
+                ", aventuriers=" + aventuriers +
+                '}';
+    }
+
 }
--- src/test/java/TresorGameUnitTests/TresorTest.java
+++ src/test/java/TresorGameUnitTests/TresorTest.java
@@ -1,11 +1,21 @@
 package TresorGameUnitTests;
 
+import TresorGame.Tresor;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class TresorTest {
     @Test
     public void testReadInputFile() {
         // Tests for reading the input file
+        Tresor tresor = new Tresor();
+        tresor.readInputFile("input.txt");
+
+        // Verify that the map and adventurers are correctly initialized
+        assertEquals(3, tresor.getMapWidth());
+        assertEquals(4, tresor.getMapHeight());
+        assertEquals(1, tresor.getAventuriers().size());
     }
 
     @Test
