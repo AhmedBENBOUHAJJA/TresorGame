4226a611fd74159fb9f86482a47ab6dc697bb5e1
Azus <a.ben.bouhajja@gmail.com>
2023-10-07 11:11:38 +0200

    adding the TresorTest.java test file (TDD)

---
+++ src/test/java/TresorGameUnitTests/TresorTest.java
@@ -0,0 +1,20 @@
+package TresorGameUnitTests;
+
+import org.junit.Test;
+
+public class TresorTest {
+    @Test
+    public void testReadInputFile() {
+        // Tests for reading the input file
+    }
+
+    @Test
+    public void testSimulateMovements() {
+        // Tests for simulating adventurers' movements
+    }
+
+    @Test
+    public void testWriteOutputFile() {
+        // Tests for writing the output file
+    }
+}
