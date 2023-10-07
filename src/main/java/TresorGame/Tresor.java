a63d066f38d53afde27a90fffcfb948e5d3fc04c
Azus <a.ben.bouhajja@gmail.com>
2023-10-07 11:26:29 +0200

    adding the Tresor.java class methods and Adventurier.java class

--- src/main/java/TresorGame/Tresor.java
+++ src/main/java/TresorGame/Tresor.java
@@ -1,10 +1,28 @@
 package TresorGame;
 
+import entity.Aventurier;
+
+import java.util.List;
+
 public class Tresor {
+    private char[][] map; // the cart
+    private List<Aventurier> aventuriers; // adventurers list
 
     // Method to read the input file and initialize the map and adventurers
 
+    public void readInputFile(String inputFile) {
+        // Read the input file and initialize the map and adventurers
+    }
+
     // Method for simulating the movements of adventurers
 
+    public void simulateMovements() {
+        // Simulate the movements of adventurers
+    }
+
     // Method to write the output file
+
+    public void writeOutputFile(String outputFile) {
+        // Write the output file
+    }
 }
