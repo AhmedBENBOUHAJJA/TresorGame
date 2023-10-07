15e47f7b6763ba4fee32eda00bac0b845dccabf3
Azus <a.ben.bouhajja@gmail.com>
2023-10-07 11:18:32 +0200

    adding the Tresor.java class file to implement the methods

---
+++ src/main/java/TresorGame/Tresor.java
@@ -0,0 +1,10 @@
+package TresorGame;
+
+public class Tresor {
+
+    // Method to read the input file and initialize the map and adventurers
+
+    // Method for simulating the movements of adventurers
+
+    // Method to write the output file
+}
